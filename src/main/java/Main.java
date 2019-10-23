1aaafe7545e53f110922fdbae252b9613c52e658
FilipAdrian <afilip@ms3-inc.com>
2019-10-23 11:51:46 +0300

    Made TcpMultiServer as Singleton

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     private static Integer SERVER_PORT = 6666;
 
     public static void main(String[] args) {
-        TcpMultiServer tcpMultiServer = new TcpMultiServer();
+        TcpMultiServer tcpMultiServer = TcpMultiServer.getInstance();
         tcpMultiServer.start(SERVER_PORT);
     }
 }
